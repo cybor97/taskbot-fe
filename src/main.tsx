970313b8314af664b501f0b95ed04ded10b4d270
cyborg97 <muhametshin.1997@gmail.com>
2024-04-28 20:26:12 +0200

    fix: prevent default scroll

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+window.addEventListener("scroll", (e) => {
+  e.preventDefault();
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
