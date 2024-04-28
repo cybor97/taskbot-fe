f2befd8ebe939f245732fea7e1ba1b6d36ed6452
cyborg97 <muhametshin.1997@gmail.com>
2024-04-28 20:36:51 +0200

    fix: prevent default scroll

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,6 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-window.addEventListener("touchmove", (e) => e.preventDefault(), {
-  passive: false,
-});
-window.scrollTo(0, 100);
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
